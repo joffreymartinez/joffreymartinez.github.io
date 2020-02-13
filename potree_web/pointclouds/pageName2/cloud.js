853b94e6851143781685f0531dc29ceb6b8947a1
joffreymartinez <46828650+joffreymartinez@users.noreply.github.com>
2020-02-13 11:57:11 -0700

    1
    a

---
+++ potree_web/pointclouds/pageName2/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 51383695,
+    "boundingBox": {
+        "lx": 700520.0,
+        "ly": 5739610.0,
+        "lz": 1018.0,
+        "ux": 700912.0,
+        "uy": 5740002.0,
+        "uz": 1410.0
+    },
+    "tightBoundingBox": {
+        "lx": 700520.7098029712,
+        "ly": 5739611.212020466,
+        "lz": 1019.3522447557151,
+        "ux": 700911.3988029712,
+        "uy": 5739894.933020466,
+        "uz": 1061.309244755715
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 3.394819498062134,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
